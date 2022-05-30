eab14e30d69166681c97e6cfc4803a8bf23239e4
Магденко Роман <mrp.mspa@playrix.com>
2022-05-30 13:58:13 +0700

    Creating a project for macos

---
+++ src/Core/MacApplication.mm
@@ -0,0 +1,10 @@
+#import <Cocoa/Cocoa.h>
+#import <AppKit/AppKit.h>
+#include "MacApplication.h"
+
+namespace Core
+{
+	void Main(int argc, const char * argv[]) {
+		NSApplicationMain(argc, argv);
+	}
+}
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "Core/MacApplication.h"
+
+int main(int argc, const char * argv[]) {
+	Core::Main(argc, argv);
+}
